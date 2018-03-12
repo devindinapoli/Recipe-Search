f55cd13b6942a63495f0509e25564d5c1a1e359c
unknown <jeasleygtr@icloud.com>
2018-03-12 11:22:44 -0400

    second push for firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAhzmSyewm-Vrt5huNVuAftTflrdMwBxEM",
+    authDomain: "project1-7a46c.firebaseapp.com",
+    databaseURL: "https://project1-7a46c.firebaseio.com",
+    projectId: "project1-7a46c",
+    storageBucket: "project1-7a46c.appspot.com",
+    messagingSenderId: "380507002059"
+  };
+  firebase.initializeApp(config);
+
+  
