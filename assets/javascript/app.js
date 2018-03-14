0a8c1976f7c0bc1ae3c492b49a0e779df5f7c959
Devin DiNapoli <devindinapoli@gmail.com>
2018-03-14 10:58:49 -0400

    Made searches pushed to database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+//Jeff's firebase
 var config = {
   apiKey: "AIzaSyAhzmSyewm-Vrt5huNVuAftTflrdMwBxEM",
   authDomain: "project1-7a46c.firebaseapp.com",
@@ -8,7 +9,7 @@ var config = {
 };
 
 firebase.initializeApp(config);
-
+//Andrew's API
 var apiKey = "_app_id=0fbe7e55&_app_key=6f1a83a5e371300fcbd1a3f859cddf85"
 var queryUrl = "https://api.yummly.com/v1/api/recipes?" + apiKey + "&maxResult=30&start=1";
 
@@ -70,6 +71,7 @@ $("#zip-button").on("click", function(event){
   var zip = $("#zip-search").val().trim();
   var newMap = $("#map").attr("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyBG5a2EUHZpq-aoy20slw4V_TpzY2ZqIMc&q=grocery+stores+" + zip)
   console.log(newMap);
+  //Devin's API
 })
     
 $("#dish-btn").on("click", function (event){
@@ -79,6 +81,8 @@ $("#dish-btn").on("click", function (event){
   var search = queryUrl;
   var ingredientArray = [];
   var excludeArray =[];
+  var database = firebase.database();
+
   if($("#search-dish").val() != "") {
     dishName = $("#search-dish").val().trim();
     dishName = dishName.split(" ").join('+');
@@ -110,7 +114,14 @@ $("#dish-btn").on("click", function (event){
   $("#include-ingredient").val("");
   $("#exclude-ingredient").val("");
   $("#map").attr("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyBG5a2EUHZpq-aoy20slw4V_TpzY2ZqIMc&q=grocery+stores+near+me");
+  
+  database.ref("/searches").push({
+    dish: dishName,
+    includedIngredients: ingredientArray,
+    excludedIngredients: excludeArray
   });
+    
+});
 
 //initializes image carousel
 $(document).ready(function(){
