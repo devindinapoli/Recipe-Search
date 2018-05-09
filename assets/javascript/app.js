d56ceac194000f034b32c17868b86e4d6ec39d46
Devin DiNapoli <devindinapoli@gmail.com>
2018-05-09 17:52:43 -0400

    Fixed header

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,184 +10,212 @@ var config = {
 
 firebase.initializeApp(config);
 
-var apiKey = "_app_id=0fbe7e55&_app_key=6f1a83a5e371300fcbd1a3f859cddf85" //Andrew's API key
-var queryUrl = "https://api.yummly.com/v1/api/recipes?" + apiKey  + "&requirePictures=true";
+var apiKey = "_app_id=0fbe7e55&_app_key=6f1a83a5e371300fcbd1a3f859cddf85"; //Andrew's API key
+var queryUrl =
+  "https://api.yummly.com/v1/api/recipes?" + apiKey + "&requirePictures=true";
 var database = firebase.database();
-var recipeArray= [];
+var recipeArray = [];
 var cardArray = [];
 var favArray = [];
 var favCardArray = [];
 
 /*-----------------------------------------------FUNCTIONAL FUNCTIONS------------------------------------------------------------------ */
-var generateCards = function(array){
-  for(var i = 0; i < array.length; i++){
-      var recipeCard =
-      $("<div class='col s12 m6 l4 recipe-card'>" +
-      "<div class='card'>" + 
-      "<div class='card-image'>" + 
-      "<img src=" + array[i].images[0].hostedMediumUrl + ">" +
-      "<span class='card-title'>" + array[i].name + "</span>" + 
-      "</div>" +
-      "<div class='card-content'>" + 
-      "<p class=recipe-p>Recipe: </p>" +
-      "<a class='link' target='_blank' href='" + array[i].source.sourceRecipeUrl + "'>" + array[i].source.sourceDisplayName + "</a>" +
-      "<br><br><i class='material-icons fav-icon' id=" + array[i].id + ">favorite</i>" +
-      "</div></div>");
-
-    }
-    $(".recipe-box").append(recipeCard);
-    //when user clicks heart icon it turns red
-    $(".fav-icon").click(function() {
-      $(this).css("color", "red");
-      $("#fav-btn").show();
-    });
-}
+var generateCards = function(array) {
+  for (var i = 0; i < array.length; i++) {
+    var recipeCard = $(
+      "<div class='col s12 m6 l4 recipe-card'>" +
+        "<div class='card'>" +
+        "<div class='card-image'>" +
+        "<img src=" +
+        array[i].images[0].hostedMediumUrl +
+        ">" +
+        "<span class='card-title'>" +
+        array[i].name +
+        "</span>" +
+        "</div>" +
+        "<div class='card-content'>" +
+        "<p class=recipe-p>Recipe: </p>" +
+        "<a class='link' target='_blank' href='" +
+        array[i].source.sourceRecipeUrl +
+        "'>" +
+        array[i].source.sourceDisplayName +
+        "</a>" +
+        "<br><br><i class='material-icons fav-icon' id=" +
+        array[i].id +
+        ">favorite</i>" +
+        "</div></div>"
+    );
+  }
+  $(".recipe-box").append(recipeCard);
+  //when user clicks heart icon it turns red
+  $(".fav-icon").click(function() {
+    $(this).css("color", "red");
+    $("#fav-btn").show();
+  });
+};
 
 // This AJAX function will only be ran after our 1st call. It's purpose is to get specific information about each recipe that came from yumCall
-var recipeCall = function(){
-  console.log("Lists Off Each Recipe As An Object From Second Ajax Request:")
-  for(var i= 0; i < recipeArray.length; i++){  
-    var newUrl = "https://api.yummly.com/v1/api/recipe/" + recipeArray[i] + "?" + apiKey;
-      $.ajax({
+var recipeCall = function() {
+  console.log("Lists Off Each Recipe As An Object From Second Ajax Request:");
+  for (var i = 0; i < recipeArray.length; i++) {
+    var newUrl =
+      "https://api.yummly.com/v1/api/recipe/" + recipeArray[i] + "?" + apiKey;
+    $.ajax({
       url: newUrl,
       method: "GET"
-    }).then(function(response){
+    }).then(function(response) {
       //console.log(response); // this will show all recipes as objs in console
       cardArray.push(response);
       generateCards(cardArray);
-    })
+    });
   }
   console.log(cardArray);
-}
+};
 
 // this ajax function is what searches through yummly's api and brings back results based on parameters given.
 // we pushed the ids of each recipe from THIS response into an array.
-var yumCall = function(search){
+var yumCall = function(search) {
   $.ajax({
     url: search,
     method: "GET"
   }).then(function(response) {
-    console.log("List Of ID's From First Ajax Request:")
-    for(var j = 0; j < response.matches.length; j++){
+    console.log("List Of ID's From First Ajax Request:");
+    for (var j = 0; j < response.matches.length; j++) {
       recipeArray.push(response.matches[j].id);
       console.log(response.matches[j].id);
     }
-    console.log("-----------------")
+    console.log("-----------------");
     recipeCall(cardArray);
     console.log(queryUrl);
   });
-}
+};
 
-function generateTable(){
+function generateTable() {
   $(".scrollbox").empty();
   var box = $("<div>");
-  box.addClass("recipe-box")
+  box.addClass("recipe-box");
   $(".scrollbox").append(box);
 }
 
 /*-----------------------------------------------EVENT HANDLERS------------------------------------------------------------------ */
-$("#zip-button").on("click", function(event){
+$("#zip-button").on("click", function(event) {
   event.preventDefault();
-  var zip = $("#zip-search").val().trim();
-  var newMap = $("#map").attr("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyBG5a2EUHZpq-aoy20slw4V_TpzY2ZqIMc&q=grocery+stores+" + zip)
+  var zip = $("#zip-search")
+    .val()
+    .trim();
+  var newMap = $("#map").attr(
+    "src",
+    "https://www.google.com/maps/embed/v1/search?key=AIzaSyBG5a2EUHZpq-aoy20slw4V_TpzY2ZqIMc&q=grocery+stores+" +
+      zip
+  );
   console.log(newMap);
   //Devin's API
-})
+});
 
-    
 // The big doozy. this on click handler will check to see what inputs the user has and has not filed
 // this function generates a var 'search' that holds all needed search parameters to be fed to our yumCall()
-$("#dish-btn").on("click", function (event){
+$("#dish-btn").on("click", function(event) {
   cardArray = [];
   recipeArray = [];
   event.preventDefault();
   var limit = 30;
   var search = "";
   var ingredientArray = [];
-  var excludeArray =[];
+  var excludeArray = [];
   var random = Math.floor(Math.random() * 150) + 1;
 
-
-// Default search conditionals
+  // Default search conditionals
   // if user has entered a dish name, execute.
-  if(($("#search-dish").val() === "") && ($("#include-ingredient").val() === "") && ($("#exclude-ingredient").val() === "")) {
-  Materialize.toast('Please Fill Out One of the Fields ', 4000);
-  $("#dish-btn").preventDefault();
+  if (
+    $("#search-dish").val() === "" &&
+    $("#include-ingredient").val() === "" &&
+    $("#exclude-ingredient").val() === ""
+  ) {
+    Materialize.toast("Please Fill Out One of the Fields ", 4000);
+    $("#dish-btn").preventDefault();
   }
-  if($("#search-dish").val() != "") {
-    dishName = $("#search-dish").val().trim();
-    dishName = dishName.split(" ").join('+');
-    search += "&q=" + dishName
+  if ($("#search-dish").val() != "") {
+    dishName = $("#search-dish")
+      .val()
+      .trim();
+    dishName = dishName.split(" ").join("+");
+    search += "&q=" + dishName;
 
-    console.log("Name of the  dish:")
+    console.log("Name of the  dish:");
     console.log(dishName);
-    console.log("-----------------")
+    console.log("-----------------");
   }
 
   // if user has included ingredients
-  if($("#include-ingredient").val() != "") {
-    var ingredients = $("#include-ingredient").val().trim();
+  if ($("#include-ingredient").val() != "") {
+    var ingredients = $("#include-ingredient")
+      .val()
+      .trim();
     ingredientArray = ingredients.split(", ");
-    for(var i = 0; i < ingredientArray.length; i++){
+    for (var i = 0; i < ingredientArray.length; i++) {
       search += "&allowedIngredient[]=" + ingredientArray[i];
 
-      console.log("Ingredients Allowed:")
+      console.log("Ingredients Allowed:");
       console.log(ingredientArray[i]);
-      console.log("-----------------")
+      console.log("-----------------");
     }
   }
 
   // if user has excluded any ingredients
-  if($("#exclude-ingredient").val() != ""){
-    var exclude = $("#exclude-ingredient").val().trim();
+  if ($("#exclude-ingredient").val() != "") {
+    var exclude = $("#exclude-ingredient")
+      .val()
+      .trim();
     excludeArray = exclude.split(", ");
-    for(var i = 0; i < ingredientArray.length; i++){
+    for (var i = 0; i < ingredientArray.length; i++) {
       search += "&excludedIngredient[]=" + excludeArray[i];
-      console.log("Ingredients Excluded:")
+      console.log("Ingredients Excluded:");
       console.log(excludeArray[i]);
-      console.log("-----------------")
+      console.log("-----------------");
     }
   }
   // if they selected a value for the course drop down
-  if($('select#course-dropdown option:selected').val() != "none") {
-    search += "&allowedCourse[]=" + $('select#course-dropdown option:selected').val();
+  if ($("select#course-dropdown option:selected").val() != "none") {
+    search +=
+      "&allowedCourse[]=" + $("select#course-dropdown option:selected").val();
   }
 
-// advance search conditionals
+  // advance search conditionals
   // checking for cuisine input
-  if($('select#cuisine-dropdown option:selected').val() != "none") {
-    search += "&allowedCuisine[]=" + $('select#cuisine-dropdown option:selected').val();
+  if ($("select#cuisine-dropdown option:selected").val() != "none") {
+    search +=
+      "&allowedCuisine[]=" + $("select#cuisine-dropdown option:selected").val();
   }
 
   // checking for diet input
-  if($('select#diet-dropdown option:selected').val() != "none") {
-    search += "&allowedDiet[]=" + $('select#diet-dropdown option:selected').val();
+  if ($("select#diet-dropdown option:selected").val() != "none") {
+    search +=
+      "&allowedDiet[]=" + $("select#diet-dropdown option:selected").val();
   }
 
   // checking for result limit value
-  if($("#limit-input").val() != "") {
+  if ($("#limit-input").val() != "") {
     search += "&maxResult=" + $("#limit-input").val();
-  }
-  else{
+  } else {
     search += "&maxResult=" + limit;
   }
 
   // check for max time input and then convert to seconds to work with yummly api
-  if($("#time-input").val() != "") {
+  if ($("#time-input").val() != "") {
     var toSec = $("#time-input").val() * 60;
     search += "&maxTotalTimeInSeconds=" + toSec;
   }
 
-  //  var search will equal a blank string unless the user inputs something to at least one field 
-  if(search != "") {
+  //  var search will equal a blank string unless the user inputs something to at least one field
+  if (search != "") {
     yumCall(queryUrl + search + "&start=" + random);
-    console.log(search)
+    console.log(search);
     generateTable();
   } else {
     // if our user has input nothing lets just return random results at defult limit (30) bc we are generous.
-     orgUrl = queryUrl + "&maxResult=" + limit + "&start=" + random;
-     yumCall(orgUrl);
-     generateTable();
+    orgUrl = queryUrl + "&maxResult=" + limit + "&start=" + random;
+    yumCall(orgUrl);
+    generateTable();
   }
   //clears all input fields upon search click
   $("#search-dish").val("");
@@ -198,41 +226,42 @@ $("#dish-btn").on("click", function (event){
   $(".diet .select-dropdown").val("Diet");
   $(".cuisine .select-dropdown").val("Cuisine");
   $(".course .select-dropdown").val("Choose your course");
-  
+
   //google maps
-  $("#map").attr("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyBG5a2EUHZpq-aoy20slw4V_TpzY2ZqIMc&q=grocery+stores+near+me");
+  $("#map").attr(
+    "src",
+    "https://www.google.com/maps/embed/v1/search?key=AIzaSyBG5a2EUHZpq-aoy20slw4V_TpzY2ZqIMc&q=grocery+stores+near+me"
+  );
 
   //hides advanced search section upon search click
   $(".advanced-search").hide();
 
   //shows message when submit button is clicked
-  Materialize.toast('Check Out the Map for Local Grocery Stores!', 4000);
+  Materialize.toast("Check Out the Map for Local Grocery Stores!", 4000);
 
-
-/*-----------------------------------------------Materialize LOGIC------------------------------------------------------------------ */
+  /*-----------------------------------------------Materialize LOGIC------------------------------------------------------------------ */
   //page moves to recipes scrollbox after submit button is clicked
-  $('html, body').animate({
-    scrollTop: $(".scrollbox").offset().top
-  }, 1000);
+  $("html, body").animate(
+    {
+      scrollTop: $(".scrollbox").offset().top
+    },
+    1000
+  );
 
   database.ref("/dishSearches").push({
-  dishName: dishName,
+    dishName: dishName
   });
-      
-
-
-
 }); // end of our dish-btn event listener
 
 //initializes image carousel
-$(document).ready(function(){
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
-  $('.carousel').carousel();
+$(document).ready(function() {
+  $(".carousel.carousel-slider").carousel({ fullWidth: true });
+  $(".carousel").carousel();
   setInterval(function() {
-    $('.carousel').carousel('next');
-    }, 3000); 
+    $(".carousel").carousel("next");
+  }, 3000);
   //initializes dropdown in general search
-  $('select').material_select();  
+  $("select").material_select();
 });
 
 //toggles advanced-search section upon click of advanced-search button
@@ -245,26 +274,26 @@ $(document).ready(function() {
   $(document).on("click", ".fav-icon", function() {
     var recipeName = $(this).attr("id");
     database.ref("/favorites").push({
-       dishName: dishName,
-      });
+      dishName: dishName
+    });
     favArray.push(recipeName);
     console.log(favArray);
   });
-})
+});
 
 $("#fav-btn").on("click", function(event) {
   event.preventDefault();
-  for(var i= 0; i < favArray.length; i++){  
-    var newUrl = "https://api.yummly.com/v1/api/recipe/" + favArray[i] + "?" + apiKey;
-      $.ajax({
+  for (var i = 0; i < favArray.length; i++) {
+    var newUrl =
+      "https://api.yummly.com/v1/api/recipe/" + favArray[i] + "?" + apiKey;
+    $.ajax({
       url: newUrl,
       method: "GET"
-    }).then(function(response){
+    }).then(function(response) {
       favCardArray.push(response);
       generateCards(favCardArray);
-    })
+    });
   }
   generateTable();
   console.log(favCardArray);
 });
-
